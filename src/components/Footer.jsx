65069336508d6529e52d529a7094ad755e3cff87
Nitin Semwal <140317834+NitinSemwal2605@users.noreply.github.com>
2025-09-14 11:44:58 +0530

    Some Setups

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-black border-t border-green-500 text-center text-green-400 py-4">
+      <p>© {new Date().getFullYear()} JS Projects Hub | Built by Nitin 🚀</p>
+    </footer>
+  );
+}
+
+export default Footer;
